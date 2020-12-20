80f331e6bc48ab6e3efce7e8f5a60a8136324c63
Konrad Eisele <konrad.eisele@aptiv.com>
2020-12-20 18:54:31 +0100

    all

--- repo/base_alias.sh
+++ repo/base_alias.sh
@@ -2,9 +2,11 @@
 
 # Option parsing
 nofetch=0
+del=0
 while [ $# -gt 0 ]; do
     case "$1" in
         -nofetch)   nofetch=1; shift ;;
+	-del)       del=1; shift ;;
         *)          break ;;
     esac
 done
@@ -20,7 +22,26 @@ function clone_alias {
     local a=${symlinkbase}/${2}.git
 
     if [ -e ${symlinkbase}/${2}.git ]; then
-	echo "!!!!!!!!!!!! ${symlinkbase}/${2}.git already exist !!!!!!!!!!!!"
+
+	if [ "$(readlink -f ${base}/${1}.git)" != "$(readlink -f ${symlinkbase}/${2}.git)" ]; then
+	    echo "${symlinkbase}/${2}.git: should point to / but point to "
+	    echo "  $(readlink -f ${base}/${1}.git) "
+	    echo "  $(readlink -f ${symlinkbase}/${2}.git)"
+
+	    if [ "${del}" == "1" ]; then
+		rm ${symlinkbase}/${2}.git
+		echo "${a} => ${n}"
+		mkdir -p $(dirname ${symlinkbase}/${2}.git)
+		ln -sf ${base}/${1}.git ${symlinkbase}/${2}.git
+	    fi
+
+	    #exit 1
+	fi
+
+	#${base}/${1}.git
+
+
+	#echo "!!!!!!!!!!!! ${symlinkbase}/${2}.git=>$(readlink -f ${symlinkbase}/${2}.git) already exist !!!!!!!!!!!!"
     else
 	echo "${a} => ${n}"
 	mkdir -p $(dirname ${symlinkbase}/${2}.git)
